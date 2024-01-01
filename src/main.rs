70a7251b5448a2a77ef6f688fbd105d801e43eca
Balagopal Kanattil <balukanattil@gmail.com>
2023-12-31 20:59:50 -0500

    Command skeleton

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
-use std::{io::{self, Write}, process::exit};
+use std::{
+    io::{self, Write},
+    process::exit,
+};
 
-const LOGO: &'static str = r#"
+const LOGO: &str = r#"
  __  __     ______     ______     _____     ______   
 /\ \_\ \   /\  __ \   /\  == \   /\  __-.  /\  == \  
 \ \____ \  \ \  __ \  \ \  __<   \ \ \/\ \ \ \  __<  
@@ -8,9 +11,11 @@ const LOGO: &'static str = r#"
   \/_____/   \/_/\/_/   \/_/ /_/   \/____/   \/_____/            
 "#;
 
+const META_PREFIX: &str = ".";
+
 fn main() {
     print_and_flush(LOGO);
-    print_and_flush("YarDB Version 0.0.1\n\n");
+    print_and_flush("\nYarDB Version 0.0.1\n\n");
 
     repl();
 }
@@ -25,16 +30,78 @@ fn repl() {
             .read_line(&mut input)
             .expect("Error while reading from standard input");
 
-        if input.trim() == ".exit" {
-            exit(0);
+        let command = input.trim();
+
+        if command.is_empty() {
+            continue;
         }
 
-        print_and_flush(&input);
+        if command.starts_with(META_PREFIX) {
+            match handle_meta_command(command) {
+                Ok(_) => {}
+                Err(e) => {
+                    print_and_flush(&format!("Error: {:?}\n", e));
+                }
+            }
+        } else {
+            handle_sql_command(command);
+        }
     }
 }
 
+fn handle_meta_command(command: &str) -> Result<(), MetaCommandHandleError> {
+    match command {
+        ".exit" => {
+            exit(0);
+        }
+        ".help" => {
+            print_and_flush("Available commands:\n");
+            print_and_flush(".exit\n");
+            print_and_flush(".help\n");
+            print_and_flush("select\n");
+            print_and_flush("insert\n");
+            Ok(())
+        }
+        _ => Err(MetaCommandHandleError::UnRecognizedCommand),
+    }
+}
+
+#[derive(Debug)]
+enum MetaCommandHandleError {
+    UnRecognizedCommand,
+}
+
+fn handle_sql_command(command: &str) {
+    match prepare_statement(command) {
+        Ok(statement) => execute_statement(&statement),
+        Err(e) => print_and_flush(&format!("Error: {}", e)),
+    }
+}
+
+fn prepare_statement(command: &str) -> Result<Statement, String> {
+    match command {
+        "insert" => Ok(Statement::Insert),
+        "select" => Ok(Statement::Select),
+        _ => Err(String::from(&format!(
+            "Unrecognized keyword at start of {}\n",
+            command
+        ))),
+    }
+}
+
+fn execute_statement(statement: &Statement) {
+    match statement {
+        Statement::Insert => print_and_flush("Executing insert statement\n"),
+        Statement::Select => print_and_flush("Executing select statement\n"),
+    }
+}
 
 fn print_and_flush(s: &str) {
     print!("{}", s);
     io::stdout().flush().unwrap();
+}
+
+enum Statement {
+    Insert,
+    Select,
 }
